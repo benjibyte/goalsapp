db2b52f2365b7b59d65a32eddfa25a7a7380caeb
benjibyte <benjipixel@proton.me>
2026-08-21 13:02:14 -0500

    Benjibyte: Durring code review, I decided to add a catch-error for not finding root

--- goals-app/src/main.jsx
+++ goals-app/src/main.jsx
@@ -4,9 +4,12 @@ import App from './App';
 //import './index.css'; // Benjibyte: can't find the module for importing the css style
 // I decided to comment out the above line because we don't need it. the styles can be 
 // injected into the final html instead of worrying about whether JS wants to import CSS values
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('rootElement element not found');
+}
 
-
-ReactDOM.createRoot(document.getElementById('root')).render( // Benjibyte: I'm not sure that "getElementByID" is the best way to connect to root.
+ReactDOM.createRoot(rootElement).render( // Benjibyte: I'm not sure that "getElementByID" is the best way to connect to root.
   <React.StrictMode>
     <App />
   </React.StrictMode>
